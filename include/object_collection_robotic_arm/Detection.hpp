4eec788f91a52d905e20014a878a47465213e3b5
Ashwin Goyal <ashgo95@terpmail.umd.edu>
2018-12-10 22:15:56 -0500

    Task: 2-15 Detection class Implementation done
    Class Detection was divided into a header file and a source file using the demo that was pushed earlier. Now, the function works as expected. Also, a method has been removed. Instead, class detection is made to inherit class KukaKinematics.

--- include/object_collection_robotic_arm/Detection.hpp
+++ include/object_collection_robotic_arm/Detection.hpp
@@ -172,46 +172,32 @@
  * @brief This is the declaration of the Detection class
  */
 
-#ifndef INCLUDE_DETECTION_HPP_
-#define INCLUDE_DETECTION_HPP_
+#ifndef INCLUDE_OBJECT_COLLECTION_ROBOTIC_ARM_KUKAKINEMATICS_DETECTION_HPP_
+#define INCLUDE_OBJECT_COLLECTION_ROBOTIC_ARM_KUKAKINEMATICS_DETECTION_HPP_
 
 #include <ros/ros.h>
-#include <iostream>
-#include <vector>
-#include <string>
-#include <opencv2/opencv.hpp>
-#include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
-#include <sensor_msgs/image_encodings.h>
+#include <image_transport/image_transport.h>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <sensor_msgs/image_encodings.h>
+#include "KukaKinematics.hpp"
 
-static const std::string OPENCV_WINDOW = "Image window";
 /*
  * @brief Detection is a class used for working with the camera in the world
  */
-class Detection {
+class Detection : public KukaKinematics {
  private:
     ros::NodeHandle n;
-    std::vector<std::vector<double>> pos;
-    image_transport::ImageTransport it_;
-    image_transport::Subscriber image_sub_;
+    image_transport::ImageTransport imgT;
+    image_transport::Subscriber imageSubscriber;
+    const std::string OPENCV_WINDOW = "Image Window";
 
  public:
     /*
      * @brief This is the constructor for the class
      */
     Detection();
-
-    /*
-     * @brief This is the first method of the class. It gives the preloaded
-     *        coordinates for a particular position index.
-     *
-     * @param This function takes the position index as input.
-     *
-     * @result This function returns the coordinates of the position index.
-     */
-    std::vector<double> getObjectCoords(const int);
 
     /*
      * @brief This is the second method of the class. It detects the position
@@ -221,7 +207,7 @@ class Detection {
      *
      * @result This function returns the position index for that object.
      */
-    int colorThresholder(const std::string);
+    std::vector<KukaKinematics::States> colorThresholder(const std::string);
     /*  
      *  
      *  
@@ -235,4 +221,4 @@ class Detection {
     ~Detection();
 };
 
-#endif  // INCLUDE_DETECTION_HPP_
+#endif  // INCLUDE_OBJECT_COLLECTION_ROBOTIC_ARM_KUKAKINEMATICS_DETECTION_HPP_
