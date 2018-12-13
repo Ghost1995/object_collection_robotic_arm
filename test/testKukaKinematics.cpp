41939b1d247a05807a0c10d310e9621315d81247
Ashwin Goyal <ashgo95@terpmail.umd.edu>
2018-12-12 20:47:04 -0500

    Task: 2-4 Developed tests for class KukaKinematics
    The dummy tests are now replaced with correct tests for the class. It is an integration test as level 1 test for any method of this class cannot be conducted.

--- test/testKukaKinematics.cpp
+++ test/testKukaKinematics.cpp
@@ -175,11 +175,41 @@
 #include <gtest/gtest.h>
 #include "KukaKinematics.hpp"
 
+trajectory_msgs::JointTrajectory command;
+
+// Create a callback function
+void callback(const trajectory_msgs::JointTrajectory & pubCommand) {
+    command = pubCommand;
+}
+
 /**
  * @brief This is the google test for the first method of the class.
  *        It is a dummy test for now.
  */
 TEST(KukaKinematicsTest, testSendRobotToPos) {
-  // Dummy test for now
-  EXPECT_TRUE(1);
+	// Initialize a KukaKinematics class object
+	KukaKinematics test;
+
+	// Initialize the node handle
+    ros::NodeHandle n;
+
+    // Initialize the home position coordinates
+    double posJoints[] = {2.918232931819958, -0.03327128068635865,
+                          -1.168222646872703, -1.2265077682929597,
+                          -0.028973511280637965, 1.9434842643815777,
+                          -1.402031709125911};
+
+	// Create a subscriber
+	auto gripperSubscriber = n.subscribe(
+		"/iiwa/PositionJointInterface_trajectory_controller/command", .10,
+                                                                    callback);
+
+	// Move the robot to a particular position
+	test.sendRobotToPos(test.HOME);
+	ros::spinOnce();
+
+	// Test if the command being sent is correct
+	EXPECT_EQ(0, command.header.seq);
+	for (auto i = 0; i < 7; i++)
+		EXPECT_EQ(posJoints[i], command.points[0].positions[i]);
 }
